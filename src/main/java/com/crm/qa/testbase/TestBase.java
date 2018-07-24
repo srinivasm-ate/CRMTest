93969d2e3d5a50d756f29acf753b3c5172b53467
mytestinghub <msrinivas.te@gmail.com>
2018-07-24 10:33:07 +0530

    CRM First Push

---
+++ src/main/java/com/crm/qa/testbase/TestBase.java
@@ -0,0 +1,16 @@
+package com.crm.qa.testbase;
+
+import java.util.Properties;
+
+import org.openqa.selenium.WebDriver;
+
+public class TestBase {
+	
+	WebDriver driver;
+	Properties prop;
+	
+	public void TestBase()
+	{
+		
+	}
+}
