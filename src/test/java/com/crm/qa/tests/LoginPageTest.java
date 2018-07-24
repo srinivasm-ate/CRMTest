a7e51e0ea200fcf4219155d711fec79e7c875221
mytestinghub <msrinivas.te@gmail.com>
2018-07-24 11:50:42 +0530

    loginpage coding done and updated

---
+++ src/test/java/com/crm/qa/tests/LoginPageTest.java
@@ -0,0 +1,57 @@
+package com.crm.qa.tests;
+
+import static org.testng.Assert.assertTrue;
+
+import java.io.IOException;
+
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+import com.crm.qa.pages.HomePage;
+import com.crm.qa.pages.LoginPage;
+import com.crm.qa.testbase.TestBase;
+
+public class LoginPageTest extends TestBase
+{
+	LoginPage lpage;
+	HomePage hpage;
+	
+	public LoginPageTest() throws IOException
+	{
+		super();
+		lpage=new LoginPage();
+	}
+	
+	@BeforeMethod
+	public void setup()
+	{
+		intilization();
+	}
+	
+	@Test(priority=0)
+	public void logotest()
+	{
+		boolean b=lpage.verifylogo();
+		assertTrue(b);
+	}
+	
+	@Test(priority=1)
+	public void logintest()
+	{
+		hpage=lpage.login(prop.getProperty("username"), prop.getProperty("password"));
+	}
+	
+	@Test(priority=2)
+	public void Titletest()
+	{
+		String title=lpage.verifytitle();
+		Assert.assertEquals(title, "#1 Free CRM for Any Business: Online Customer Relationship Software");
+	}
+	
+	@AfterMethod
+	public void teardown()
+	{
+		driver.quit();
+	}
+	
+}
